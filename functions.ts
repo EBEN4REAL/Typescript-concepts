ca13e04fca03d37dd3963ddfed1beb823971b217
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-05-05 16:01:37 +0100

    ..

--- functions.ts
+++ functions.ts
@@ -1,7 +1,22 @@
 
-function getSum(num1, num2){
+function getSum(num1:number, num2:number):number {
     return num1 + num2;
 }
 
+let mySum = function(num1:any, num2:any):number {
+    if(typeof num1 == 'string'){
+        num1 = parseInt(num1);
+    }
+    if(typeof num2== 'string'){
+        num1 = parseInt(num2);
+    }
+    return num1 + num2;
+}
 
-console.log(getSum(1,4));
+// console.log(mySum("3",5))
+
+function getName(firstName:string, lastName?:string):string {
+    return firstName + ' ' + lastName;
+}
+
+console.log(getName("John", "Doe"));
