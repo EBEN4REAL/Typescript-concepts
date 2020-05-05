2c9de6293f17864e4e387f8991652b1d08fdc0b3
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-05-05 15:38:33 +0100

    ..

--- types.ts
+++ types.ts
@@ -1,14 +1,19 @@
+ 
 
 let someStr: string;
 let myNum: number;
 let myBool: true
 let age: any;
-let strArr: string[]
+let strArr: Array<string>;
+let numsArr: number[];
+let tupsArr: [string, number, boolean]
 
 myNum = 5;
 someStr = "Welcome to Typescript";
 myBool = true;
 age = 77;
-strArr = ["Een", "Josh"]
+strArr = ["Een", "Josh"];
+numsArr = [1,2,3,4];
+tupsArr = ["Eben", 29, true];
 
-console.log(someStr, myNum, myBool, age, strArr);
+console.log(someStr, myNum, myBool, age, strArr, numsArr, tupsArr);
