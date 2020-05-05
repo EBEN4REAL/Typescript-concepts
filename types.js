91ea2d6bae050975599cad461c2f1f18c8732d02
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-05-05 15:31:28 +0100

    ..

--- types.js
+++ types.js
@@ -1,1 +1,11 @@
-console.log("Hello Eben!");
+var someStr;
+var myNum;
+var myBool;
+var age;
+var strArr;
+myNum = 5;
+someStr = "Welcome to Typescript";
+myBool = true;
+age = 77;
+strArr = ["Een", "Josh"];
+console.log(someStr, myNum, myBool, age, strArr);
--- types.ts
+++ types.ts
@@ -1,1 +1,14 @@
-console.log("Hello Eben!");
+
+let someStr: string;
+let myNum: number;
+let myBool: true
+let age: any;
+let strArr: string[]
+
+myNum = 5;
+someStr = "Welcome to Typescript";
+myBool = true;
+age = 77;
+strArr = ["Een", "Josh"]
+
+console.log(someStr, myNum, myBool, age, strArr);
